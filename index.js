99a35ffa9be94ca4693e94cdf23856fffb42f43b
DoubleMalt <christoph@web.crofting.com>
2014-03-16 10:43:48 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,140 @@
+var express = require('express');
+var _ = require('lodash');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+
+var UserAPIServer = function(options)
+{
+    var userStore = options["userStore"];
+    var userStoreInitializer = options["user_store_initializer"];
+
+    // better invalidate cookies on server reboot than having an insecure default
+    var cookieSecret = options["cookie_secret"] || Math.random().toString(36).slice(2);
+
+    var webServer = options["server"] || express();
+
+    webServer.use(express.cookieParser(cookieSecret));
+    webServer.use(express.cookieSession());
+    webServer.use(express.bodyParser());
+    webServer.use(passport.initialize());
+    webServer.use(passport.session());
+
+    var isAuthenticated = function(req, res, next)
+    {
+        if(req.isAuthenticated())
+        {
+            next();
+        }
+        else
+        {
+            res.status(401).send('Not authenticated');
+        }
+    };
+
+    var isAdmin = function(req, res, next)
+    {
+        if(req.isAuthenticated() && req.user.isAdmin)
+        {
+            next();
+        }
+        else
+        {
+            res.status(401).send('Not authenticated');
+        }
+    };
+
+
+    passport.use(new LocalStrategy(
+        function(username, password, done) {
+
+            if(userStore.isInitialized())
+            {
+
+                if(userStore.authorize(username, password))
+                {
+                    return done(null, userStore.getUsers()[username]);
+                }
+                else
+                {
+                    return done(null, false, { message: 'Incorrect credentials.' });
+                }
+            }
+            else
+            {
+                var user = userStoreInitializer.initialize();
+                if(user)
+                {
+                    return done(null, user);
+                }
+                else
+                {
+                    return done(null, false, { message: 'Incorrect credentials.' });
+                }
+            }
+        }
+    ));
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function(id, done) {
+        done(null, userStore.getUsers()[id]);
+    });
+
+
+    webServer.get('/api/v1/currentUser', isAuthenticated, function(req, res){
+
+        var user = req.user;
+
+        if(user)
+        {
+            var body = JSON.stringify(user);
+            res.setHeader('Content-Type', 'application/json');
+            res.setHeader('Content-Length', body.length);
+            res.end(body);
+        }
+        else
+        {
+            res.status(404).send('Not found');
+        }
+    });
+
+    webServer.post('/login', passport.authenticate('local'), function(req, res){
+        res.end('{"success": true}');
+    });
+
+    webServer.get('/api/v1/users', isAdmin, function(req, res){
+        var body = JSON.stringify(_.values(userStore.getUsers()));
+        res.setHeader('Content-Type', 'application/json');
+        res.setHeader('Content-Length', body.length);
+        res.end(body);
+    });
+    webServer.get('/api/v1/users/:user_id', isAdmin, function(req, res){
+        var body = JSON.stringify(userStore.getUsers()[req.param('user_id')]);
+        res.setHeader('Content-Type', 'application/json');
+        res.setHeader('Content-Length', body.length);
+        res.end(body);
+    });
+    webServer.post('/api/v1/users/:user_id', isAdmin, function(req, res){
+        var user_id = req.param('user_id');
+        var client_user = req.body;
+        userStore.updateUser(client_user);
+
+        var body = JSON.stringify(client_user);
+
+        res.setHeader('Content-Type', 'application/json');
+        res.setHeader('Content-Length', body.length);
+        res.end(body);
+    });
+
+
+    this.userApiServer = webServer
+};
+
+module.exports = {
+    createServer: function(options){
+        var server = new UserAPIServer(options);
+        return server.userApiServer;
+    }
+};
